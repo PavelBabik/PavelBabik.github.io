2326d94e3374178a925ab87c942ccb2c5df9bfbe
Pavlo Babik <olya@Pashas-MacBook-Air.local>
2020-06-06 11:49:43 +0300

    Submit Event

--- JSAdvanced/Film_Server/client/src/components/forms/__tests__/LoginForm.js
+++ JSAdvanced/Film_Server/client/src/components/forms/__tests__/LoginForm.js
@@ -4,9 +4,16 @@ import LoginForm from "../LoginForm";
 import { MemoryRouter } from "react-router-dom";
 import { toHaveAttribute, toHaveTextContent } from "@testing-library/jest-dom/matchers"
 // import { getQueriesForElement } from "@testing-library/dom"
-import {render} from "@testing-library/react"
+import {render,fireEvent} from "@testing-library/react"
 
 expect.extend({toHaveAttribute, toHaveTextContent})
+
+const submit = jest.fn(() => Promise.resolve())
+
+const data = {
+    email: "test@mail.com",
+    password: "mypass",
+  }
 
 test("LoginForm should render correct", () => {
   const { getByLabelText } = render(
@@ -27,13 +34,31 @@ test("First",() => {
     debug()
 })
 
-test("LoginForm should render correct", () => {
+test("LoginForm snaphot", () => {
     const { container } = render(
       <MemoryRouter>
         <LoginForm />
       </MemoryRouter>
     );
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test("LoginForm submit", () => {
+    const { debug, getByLabelText, getByTestId } = render(
+      <MemoryRouter>
+        <LoginForm submit={submit}/>
+      </MemoryRouter>
+    );
+  
+    const emailElement = getByLabelText(/email/i);
+    const passwordElement = getByLabelText(/password/i);
+    fireEvent.change(emailElement, {target: {value: data.email}})
+    fireEvent.change(passwordElement, {target: {value: data.password}})
+  
+    const loginButton = getByTestId("login")
+    fireEvent.click(loginButton)
+    expect(submit).toHaveBeenCalledTimes(1);
+    expect(submit).toHaveBeenCalledWith(data)
   })
 
 
