0ed239797c89815f970e1f67c9871919bbed64ce
Pavlo Babik <olya@Pashas-MacBook-Air.local>
2020-10-04 07:36:52 +0300

    StellaNEW REFRESH

--- Stella2/js/main.js
+++ Stella2/js/main.js
@@ -41,6 +41,43 @@ $(document).ready(function () {
   });
 });
 
+const animItems = document.querySelectorAll("._anim-items");
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll(params) {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffSet = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        let animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffSet - animItemPoint &&
+        pageYOffset < animItemOffSet + animItemHeight
+      ) {
+        animItem.classList.add("_active");
+      } else {
+        if (!animItem.classList.contains("_anim-nohide")) {
+          animItem.classList.remove("_active");
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+  animOnScroll();
+}
+
 $(document).ready(function () {
   $(".header__menu").click(function (event) {
     $(".header__items").toggleClass("show");
